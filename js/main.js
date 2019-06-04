5f6c6339cfa9a9df01e7707c95cfdfa060925ed2
Alex-Kun <alejandro.quiros@live.u-tad.com>
2019-06-04 21:54:21 +0200

    all

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(function(){
   $pkmn_list = [];
+  var frame1 = true;
+  //var for frames
+  var first_frame = "";
+  var second_frame = "";
+
   for (var i = 1; i < 494; i++) {
     $pkmn_list.push("images/pkmn/"+i+".png");
   }
@@ -20,6 +25,8 @@ $(function(){
     "images/route/sand.png"
   ];
   $actual_pkmn = $pkmn_list[18];
+  $actual_second_frame = "images/pkmn/frame2/19.png";
+  second_frame = $actual_second_frame;
   $actual_trainer = $trainer_list[7];
   $actual_map = $route_list[0];
   //-----------------------------------
@@ -41,9 +48,25 @@ $(function(){
     $('.gallery.'+$data).removeClass('none');
   });
 
-  function loopImg(array, gallery){
+  function setFrameVars(data){
+    //var for frames
+    first_frame = $('.char2 img').attr('src');
+    if(data != null){
+      second_frame = data;
+    }
+  }
+
+  function loopImg(array, gallery, bool){
     for (var i = 0; i < array.length; i++) {
-      $img = "<img src='"+array[i]+"'>";
+
+      if(bool){
+        var parts = array[i].split("/");
+        var new_src = parts[0]+"/"+parts[1]+"/"+"frame2/"+parts[2];
+
+        $img = "<img src='"+array[i]+"' data-frame='"+new_src+"'>";
+      } else{
+        $img = "<img src='"+array[i]+"'>";
+      }
       $(gallery+" .scroll").append($img);
     }
   }
@@ -51,21 +74,40 @@ $(function(){
     $(gallery+" img").click(function(){
       $this = $(this);
       $src = $this.attr('src');
+      $data = $this.data('frame');
       if(gallery == ".escenario"){
         $('.card').css('background-image', 'url('+$src+')');
       } else{
         $(elmToChange+" img").attr('src', $src);
+        setFrameVars($data);
       }
+
       $('.gallery').addClass('none');
     });
   }
 
-  loopImg($pkmn_list, '.gallery.pkmn');
-  loopImg($trainer_list, '.gallery.entrenador');
-  loopImg($route_list, '.gallery.escenario');
+  function pkmnAnimation(){
+    setTimeout(function () {
+      if(frame1){
+        $('.char2 img').attr('src', first_frame);
+        frame1 = false;
+      } else{
+        $('.char2 img').attr('src', second_frame);
+        frame1 = true;
+      }
+
+      pkmnAnimation();
+    }, 500);
+  }
+
+  loopImg($pkmn_list, '.gallery.pkmn', true);
+  loopImg($trainer_list, '.gallery.entrenador', false);
+  loopImg($route_list, '.gallery.escenario', false);
 
   changeSprite('.entrenador', '.char1');
   changeSprite('.pkmn', '.char2');
   changeSprite('.escenario', 'card');
+  setFrameVars();
+  pkmnAnimation();
 
 });
