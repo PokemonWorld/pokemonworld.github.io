cb2b1f025fffd6f7a2a95b65780f0ff3ef4d0cb2
Alejandro Quiros <alejandro.quiros-navarro@mondiamedia.com>
2019-06-18 11:52:32 +0200

    a

--- fit/js/main.js
+++ fit/js/main.js
@@ -1,17 +1,4 @@
 $(function(){
-  function setHeightToBlocks(){
-    // var start_h = 0;
-    // $('.offer-block').each(function(){
-    //   $this = $(this);
-    //   $height = $this.height();
-    //   console.log($height);
-    //   if(start_h < $height){
-    //     start_h = $height;
-    //   }
-    // });
-    // start_h += 40;
-    // $('.offer-block').css('height', start_h+"px");
-    // console.log(start_h);
-  }
-  setHeightToBlocks();
+  var vid = document.getElementById("landing-video");
+  vid.play();
 });
