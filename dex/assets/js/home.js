d25e89af10ee39b39694764bf79a9bf1650e1004
Alejandro Quiros <alejandro.quiros-navarro@mondiamedia.com>
2019-08-02 13:32:30 +0200

    dex

--- dex/assets/js/home.js
+++ dex/assets/js/home.js
@@ -1,10 +1,13 @@
 $(function() {
   $('.pokemon-list-home').hover(function() {
     $(this).addClass('nooverflow');
+    $('.pokemon-list-home').addClass('dark');
+    $(this).removeClass('dark');
     $(this).find('.pokemon-list-home-image').addClass('hovered');
   }, function() {
     // on mouseout, reset the background colour
     $(this).removeClass('nooverflow');
+    $('.pokemon-list-home').removeClass('dark');
     $(this).find('.pokemon-list-home-image').removeClass('hovered');
   });
 });
